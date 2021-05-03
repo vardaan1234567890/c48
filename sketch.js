07d1247ade2d2bdfafda0480128380a6f2c15a8d
vardaan1234567890 <73224389+vardaan1234567890@users.noreply.github.com>
2021-05-03 18:47:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+var car1;
+var car2;
+var car3;
+var car4;
+var cars;
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
